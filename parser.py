e9583537ac7a7458df3f5944a3edd23718170952
jnphilipp <mail@jnphilipp.org>
2017-12-03 08:19:38 +0100

    Update BaseParser.

--- parser.py
+++ parser.py
@@ -6,26 +6,32 @@ import urllib.request
 
 
 class BaseParser:
-    user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0'
+    DEFAULT_USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:41.0) ' + \
+        'Gecko/20100101 Firefox/41.0'
 
-    def fetch(self, url, quote=True, data=None, user_agent=False, extra_headers={}):
+    def __init__(self, user_agent=None, *args, **kwargs):
+        self.user_agent = user_agent
+
+    def fetch(self, url, data=None, user_agent=None):
         headers = {}
         if user_agent:
+            headers = {'User-Agent': user_agent}
+        elif self.user_agent:
             headers = {'User-Agent': self.user_agent}
-        if extra_headers:
-            headers = dict(headers.items(), **dict(extra_headers.items()))
         if data:
             data = urllib.parse.urlencode(data).encode('utf-8')
-            request = urllib.request.Request(urllib.parse.quote(url, ':/?=&') if qoute else url, data, headers)
-        else:
-            request = urllib.request.Request(urllib.parse.quote(url, ':/?=&') if quote else url, headers=headers)
+
+        request = urllib.request.Request(urllib.parse.quote(url, ':/?='),
+                                         data if data else None, headers)
         with urllib.request.urlopen(request) as response:
             content = response.read()
             if 'charset' in response.getheader('Content-Type'):
-                encoding = response.getheader('Content-Type').split('charset=', 1)[-1].split('"', 1)[0]
+                encoding = response.getheader('Content-Type') \
+                    .split('charset=', 1)[-1].split('"', 1)[0]
                 if encoding:
                     content = content.decode(encoding)
-            if 'application/json' in response.getheader('Content-Type'):
+            if 'application/json' in response.getheader('Content-Type') or \
+                    'text/json' in response.getheader('Content-Type'):
                 content = json.loads(content.decode('utf-8'))
         return content
 
