97ea213d0f7058624ad2c44526b987c1109c6ce7
jnphilipp <mail@jnphilipp.org>
2017-12-03 08:19:15 +0100

    Add Singleton base class.

--- base.py
+++ base.py
@@ -8,3 +8,13 @@ def lookahead(iterable):
         yield last, True
         last = val
     yield last, False
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args,
+                                                                 **kwargs)
+        return cls._instances[cls]
